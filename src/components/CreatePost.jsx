6f6c9efe43066a2dd1ba9489d115e0b7fc3870f5
claytonweber <claytronweber@gmail.com>
2023-06-20 00:58:16 -0600

    started register

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function CreatePost(props) {
     //gotta figure out where to put this
